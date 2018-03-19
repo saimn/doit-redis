a8c9ff8a862db1e696f4aa8e0c3b134440f64c6e
Simon Conseil <contact@saimon.org>
2018-03-19 11:09:01 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='doit-redis',
+    version='0.1',
+    license='MIT',
+    author='Simon Conseil',
+    author_email='contact@saimon.org',
+    url='http://github.com/saimn/doit-report',
+    description='Redis backend for doit',
+    long_description=open('README.md').read(),
+    py_modules=['doit_redis'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    python_requires='>=3.5',
+    keywords=['doit'],
+    install_requires=['doit', 'redis'],
+    entry_points={
+        'doit.BACKEND': [
+            'redis = doit_redis:RedisDB'
+        ]
+    },
+)
